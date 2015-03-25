736db978be74fa8b6fe018c03dbff84f707541c7
Nicolas CARRIER <carrier.nicolas0@gmail.com>
2015-03-25 18:04:38 +0100

    read the vehicle name from the config when creating a release
    that is, from the vehicle_name at the workspace's root if it exists,
    defaulting to "obstination".

--- release.sh
+++ release.sh
@@ -10,19 +10,14 @@ export CARINO_VERSION_TYPE="release"
 . config/build_config
 
 function usage {
-	echo "usage : release.sh VEHICLE_NAME VERSION"
+	echo "usage : release.sh VERSION"
 	exit $1
 }
 
 if [ -z "$1" ]; then
 	usage 1
 fi
-vehicle_name=$1
-
-if [ -z "$2" ]; then
-	usage 1
-fi
-version=$2
+version=$1
 
 # be sure the manifest has no local modifications
 cd .repo/manifests
@@ -35,11 +30,11 @@ fi
 cd -
 
 # generate, commit and tag the manifest for the current software's state
-repo manifest --revision-as-HEAD -o .repo/manifests/${vehicle_name}.xml
+repo manifest --revision-as-HEAD -o .repo/manifests/${CARINO_VEHICLE}.xml
 cd .repo/manifests
-git add ${vehicle_name}.xml
-git commit -m "prepare version ${version} for vehicle ${vehicle_name}"
-git tag ${vehicle_name}-${version}-tag
+git add ${CARINO_VEHICLE}.xml
+git commit -m "prepare version ${version} for vehicle ${CARINO_VEHICLE}"
+git tag ${CARINO_VEHICLE}-${version}-tag
 cd -
 
 if [ ${dirty} = TRUE ]; then
@@ -57,18 +52,18 @@ rm -rf ${OUT_DIR}
 sudo CARINO_VERSION_TYPE="release" ./gen_sd.sh
 
 mkdir -p ${VERSIONS_DIR}
-mv ${OUT_DIR}/carino.img ${VERSIONS_DIR}/${vehicle_name}-${version}.img
+mv ${OUT_DIR}/carino.img ${VERSIONS_DIR}/${CARINO_VEHICLE}-${version}.img
 
 # create the release archive
-tar cvjf ${VERSIONS_DIR}/${vehicle_name}-${version}.tar.bz2 \
-	${VERSIONS_DIR}/${vehicle_name}-${version}.img
+tar cvjf ${VERSIONS_DIR}/${CARINO_VEHICLE}-${version}.tar.bz2 \
+	${VERSIONS_DIR}/${CARINO_VEHICLE}-${version}.img
 
 # cleanup
-rm -f ${VERSIONS_DIR}/${vehicle_name}-${version}.img
+rm -f ${VERSIONS_DIR}/${CARINO_VEHICLE}-${version}.img
 
 # version the new version (funny isn't it ?)
 cd ${VERSIONS_DIR}
-git add ${vehicle_name}-${version}.tar.bz2
-git commit -m "${vehicle_name} version ${version}"
+git add ${CARINO_VEHICLE}-${version}.tar.bz2
+git commit -m "${CARINO_VEHICLE} version ${version}"
 cd -
 
