7a8d635214e5838bea3a2e0cd7d845da583cfe08
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-11-02 07:07:29 +0100

    alchemy-base build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export ALCHEMY_WORKSPACE_DIR=${PWD}
+export ALCHEMY_TARGET_SCAN_PRUNE_DIRS=${ALCHEMY_WORKSPACE_DIR}
+export ALCHEMY_TARGET_SCAN_ADD_DIRS=${ALCHEMY_WORKSPACE_DIR}/packages
+export ALCHEMY_HOME=alchemy
+if [ -z $ALCHEMY_USE_COLORS ]
+then
+	export ALCHEMY_USE_COLORS=1
+fi
+
+# once we'll have multiple products or variants, we will have to specify the
+# next two, via command line parameters
+export TARGET_PRODUCT=carino
+export TARGET_PRODUCT_VARIANT=obstination
+
+export ALCHEMY_TARGET_CONFIG_DIR=${ALCHEMY_WORKSPACE_DIR}/products/${TARGET_PRODUCT}/${TARGET_PRODUCT_VARIANT}/config
+export ALCHEMY_TARGET_OUT=${ALCHEMY_WORKSPACE_DIR}/out/${TARGET_PRODUCT}-${TARGET_PRODUCT_VARIANT}
+
+if [ -n "$(which ccache)" ]
+then
+	export PATH=/usr/lib/ccache:$PATH
+	export USE_CCACHE=1
+fi
+
+${ALCHEMY_HOME}/scripts/alchemake "$@"
